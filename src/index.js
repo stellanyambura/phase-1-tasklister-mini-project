59025035abf41344cf00b85a5dad2c5d9566b0dc
Stray Gee <stellanyambura>
2022-12-08 11:38:45 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+  document.addEventListener("DOMContentLoaded", () => {
+    // your code here
+    let form= document.querySelector('form')
+    form.addEventListener('submit',(e)=> {
+      e.preventDefault()
+      makeTask(e.target['new-task-description'].value)
+      form.reset()
+    })
+  });
+  
+  function makeTask (task){
+    let li = document.createElement('li');
+    let button = document.createElement('button')
+    button.addEventListener('click', deleteTask)
+    button.textContent = "DELETE"
+    li.textContent = task + " "
+    li.appendChild(button)
+    document.getElementById("tasks").appendChild(li)
+  }
+  function deleteTask(e){
+    e.target.parentNode.remove
+  
+  }
